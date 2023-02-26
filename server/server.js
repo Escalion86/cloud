959b21e7bfb582fdfe7e1a7ce0a658fb1b4bf2bf
Escalion <escalion86@gmail.com>
2023-02-26 20:43:55 +0700

    added error handler

--- server/server.js
+++ server/server.js
@@ -40,6 +40,11 @@ app.use(
   })
 ) // Allows incoming requests from any I
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 var path
 var urls = []
 
