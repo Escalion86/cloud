8db475544b2e1c8e50b1d4597711f0a09b00f3db
Escalion <escalion86@gmail.com>
2023-02-27 22:08:44 +0700

    added resizing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const multer = require('multer')
 const cors = require('cors')
 const fs = require('fs')
+const sharp = require('sharp')
+const path = require('path')
+
 const { v4: uuidv4 } = require('uuid')
 require('dotenv').config()
 // console.log(process.env) // remove this after you've confirmed it is working
@@ -45,22 +48,26 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 })
 
-var path
+var pathFolder
 var urls = []
 
 // Start by creating some disk storage options:
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
     const { project, folder, password } = req.body
-    console.log('project', project)
-    console.log('folder', folder)
+    // console.log('project', project)
+    // console.log('folder', folder)
     // console.log('password', password)
     // console.log('process.env.PASSWORD', process.env.PASSWORD)
     // if (!!password && password === process.env.PASSWORD) {
-    path = `${project}/${folder}`
-    const serverPath = `${__dirname}/../client/uploads/${path}`
+    pathFolder = `${project}/${folder}`
+    const serverPath = `${__dirname}/../client/temp`
+    // const serverPath = `${__dirname}/../client/uploads/${pathFolder}`
     // console.log('req.headers', req.headers)
-    fs.mkdirSync(serverPath, { recursive: true })
+    // fs.mkdirSync(serverPath, { recursive: true })
+    fs.mkdirSync(`${__dirname}/../client/uploads/${pathFolder}`, {
+      recursive: true,
+    })
     callback(null, serverPath)
     // } else {
     //   callback('Wrong password')
@@ -71,7 +78,7 @@ const storage = multer.diskStorage({
     const randomPart = uuidv4()
     const extension = file.mimetype.split('/')[1]
     const newFileName = `${randomPart}.${extension}`
-    urls.push(`${path}/${newFileName}`)
+    urls.push(`${pathFolder}/${newFileName}`)
     callback(null, newFileName)
   },
   // Sets saved filename(s) to be original filename(s)
@@ -80,6 +87,20 @@ const storage = multer.diskStorage({
 // Set saved storage options:
 var maxSize = 100 * 1024 * 1024 * 1024
 const upload = multer({ storage, limits: { fileSize: maxSize } })
+
+// const resizer = new MulterResizer({
+//   multer: upload,
+//   tasks: [
+//     {
+//       resize: {
+//         width: 2400,
+//         height: 2400,
+//         format: 'jpg',
+//         // suffix: 'resized-big'
+//       },
+//     },
+//   ],
+// })
 
 // app.get('*', function (req, res) {
 //   const protocol = req.protocol
@@ -93,7 +114,20 @@ const upload = multer({ storage, limits: { fileSize: maxSize } })
 //   res.send(responseString)
 // })
 
-app.post('/api', upload.array('files'), (req, res) => {
+// const imageUpload = async (req) => {
+//   ​
+//       const formattedFileName = req.file.originalname.split(' ').join('-'); //replace space with -
+//       try {
+//           await sharp(req.file.buffer)
+//           .resize({with:800, height:600}) //max width = 800 or height = 600
+//           .toFile('./uploads/'+ formattedFileName); //upload to /upload folder
+//   ​
+//       } catch (error) {
+//           console.log(error);
+//       }
+//   }
+
+app.post('/api', upload.single('files'), async (req, res) => {
   // console.log('req.body', req.body)
   // const protocol = req.protocol
   // const host = req.hostname
@@ -105,10 +139,26 @@ app.post('/api', upload.array('files'), (req, res) => {
   // console.log('req.headers2', req.headers)
   // console.log(req.body) // Logs form body values
   // console.log(req.files) // Logs any files
+  // await imageUpload(req)
+  const { filename: image } = req.file
+  // sharp.cache(false)
+  // console.log('req.file.destination', req.file.destination)
+  await sharp(req.file.path)
+    .resize(2400, 2400, {
+      fit: 'inside',
+      withoutEnlargement: true,
+    })
+    .jpeg({ quality: 90 })
+    .toFile(
+      path.resolve(req.file.destination, '../uploads/', pathFolder, image)
+    )
+  fs.unlinkSync(req.file.path)
+
   const urlsToSend = urls.map(
     (url) => `https://escalioncloud.ru/uploads/${url}`
   )
   urls = []
+  console.log('urlsToSend', urlsToSend)
   res.json(urlsToSend)
   // res.json(true)
 })
