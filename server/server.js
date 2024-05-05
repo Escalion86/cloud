d67b14f0046dd8598eab3894fb35d523aed61a0e
Escalion <escalion86@gmail.com>
2024-05-06 02:11:01 +0700

    cors -

--- server/server.js
+++ server/server.js
@@ -15,37 +15,36 @@ require('dotenv').config()
 
 var app = express()
 app.use(
-  cors()
-  //   {
-  //   allowedHeaders: [
-  //     // 'authorization',
-  //     'Access-Control-Allow-Headers',
-  //     'Access-Control-Allow-Origin',
-  //     // 'Access-Control-Request-Method',
-  //     // 'Access-Control-Request-Headers',
-  //     // 'Origin',
-  //     // 'Cache-Control',
-  //     // 'Content-Type',
-  //     // 'X-Token',
-  //     // 'X-Refresh-Token',
-  //   ], // you can change the headers
-  //   // exposedHeaders: ['authorization'], // you can change the headers
-  //   origin: [
-  //     'http://www.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'http://xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'https://www.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'https://xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'http://www.nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'http://nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'https://www.nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'https://nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
-  //     'escalioncloud.ru',
-  //     'www.escalioncloud.ru',
-  //     'http://localhost:3000',
-  //   ],
-  //   // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   // preflightContinue: false,
-  // }
+  cors({
+    allowedHeaders: [
+      // 'authorization',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin',
+      // 'Access-Control-Request-Method',
+      // 'Access-Control-Request-Headers',
+      // 'Origin',
+      // 'Cache-Control',
+      // 'Content-Type',
+      // 'X-Token',
+      // 'X-Refresh-Token',
+    ], // you can change the headers
+    // exposedHeaders: ['authorization'], // you can change the headers
+    origin: [
+      'http://www.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'http://xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'https://www.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'https://xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'http://www.nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'http://nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'https://www.nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'https://nrsk.xn--80aaennmesfbiiz1a7a.xn--p1ai',
+      'escalioncloud.ru',
+      'www.escalioncloud.ru',
+      'http://localhost:3000',
+    ],
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // preflightContinue: false,
+  })
 ) // Allows incoming requests from any I
 
 app.use(function (err, req, res, next) {
