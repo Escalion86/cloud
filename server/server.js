d73f741d9ddf9ee5a8f7add3a87c3a50dfbcc873
Escalion <escalion86@gmail.com>
2024-05-06 11:42:49 +0700

    cors update

--- server/server.js
+++ server/server.js
@@ -160,7 +160,7 @@ const upload = multer({ storage, limits: { fileSize: maxSize } })
 //       }
 //   }
 
-app.post('/api/files', (req, res) => {
+app.get('/api/files', (req, res) => {
   const directory = req.query.directory
   console.log('directory :>> ', directory)
   const directoryPath = `${__dirname}/../client/uploads/${directory}` // Specify the directory path here
