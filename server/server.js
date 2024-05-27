f458bea565ca4115ebb2630afbf2550d51e3f361
Escalion <escalion86@gmail.com>
2024-05-27 20:25:48 +0700

    Try to fix access origin

--- server/server.js
+++ server/server.js
@@ -63,14 +63,14 @@ app.use(
       'www.escalioncloud.ru',
       'http://localhost:3000',
     ],
-    // headers: [
-    //   'Origin',
-    //   // 'X-Requested-With',
-    //   'Content-Type',
-    //   'Accept',
-    //   'Access-Control-Allow-Headers',
-    //   'Access-Control-Allow-Origin',
-    // ],
+    headers: [
+      'Origin',
+      // 'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: true,
   })
